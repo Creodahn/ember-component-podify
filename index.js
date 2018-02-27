f42e14c693b28c9d84447f0c645b454d96fc89e4
Justin Drew <justin.a.drew@gmail.com>
2018-02-26 17:52:14 -0700

    make project take a directory as an argument, rather than hardcoding it in the script

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
-var fs = require('graceful-fs'),
-    mkdirp = require('mkdirp'),
-    path = require('path'),
-    mv = require('mv');
+const colors = require('colors'),
+      figures = require('figures'),
+      fs = require('graceful-fs'),
+      mkdirp = require('mkdirp'),
+      path = require('path'),
+      mv = require('mv'),
+      args = require('minimist')(process.argv);
 
-//Change these paths to reflect the correct path for your project
-var component_dir = path.resolve('..', 'municity-connect-ember', 'app', 'components'),
-    component_templates = path.resolve(component_dir, '..', 'templates', 'components');
+console.log('starting...');
 
-console.log(component_dir);
+if(args.dir) {
+  const componentDir = path.resolve(args.dir, 'app', 'components'),
+      componentTemplates = path.resolve(componentDir, '..', 'templates', 'components');
 
-fs.readdir(component_dir, function(err, list) {
-  for(var i = 0; i < list.length; i++) {
-    if(list[i].indexOf('.js') > -1) {
-      mv(path.resolve(component_dir, list[i]), path.resolve(component_dir, list[i].replace('.js', ''), 'component.js'), {mkdirp: true}, function(err) {
-        if(err) {
-          console.log('error!', err);
+  fs.readdir(componentDir, function(err, list) {
+    if(err) {
+      console.log(colors.red('error reading component directory!\n'), err);
+    } else if(list) {
+      console.log('\nmoving component javascript...\n');
+
+      list.map((item) => {
+        let result = `${figures.tick} moved`;
+
+        if(item.indexOf('.js') > -1) {
+          mv(path.resolve(componentDir, item), path.resolve(componentDir, item.replace('.js', ''), 'component.js'), { mkdirp: true }, function(err) {
+            if(err) {
+              console.log(colors.red('error!'), err);
+            }
+          });
+        } else {
+          result = `${figures.cross} not moved`;
         }
-      })
-      console.log(list[i]);
-    }
-  }
-});
 
-fs.readdir(component_templates, function(err, list) {
-  for(var i = 0; i < list.length; i++) {
-    if(list[i].indexOf('.hbs') > -1) {
-      mv(path.resolve(component_templates, list[i]), path.resolve(component_dir, list[i].replace('.hbs', ''), 'component.hbs'), {mkdirp: true}, function(err) {
-        if(err) {
-          console.log('template copy error!', err);
-        }
+        console.log(path.resolve(componentDir, item), result === 'moving...' ? colors.green(result) : colors.red(result));
       });
+    } else {
+      console.log('no results in component directory');
     }
-  }
-});
+  });
+
+  fs.readdir(componentTemplates, function(err, list) {
+    if(err) {
+      console.log(colors.red('error reading component template directory!\n'), err);
+    } else if(list) {
+      console.log('\nmoving component templates...\n');
+
+      list.map((item) => {
+        let result = `${figures.tick} moved`;
+
+        if(item.indexOf('.hbs') > -1) {
+          mv(path.resolve(componentTemplates, item), path.resolve(componentDir, item.replace('.hbs', ''), 'template.hbs'), { mkdirp: true }, function(err) {
+            if(err) {
+              console.log(colors.red('template copy error!'), err);
+            }
+          });
+
+
+        } else {
+          result = `${figures.cross} not moved`;
+        }
+
+        console.log(path.resolve(componentTemplates, item), result === 'moving...' ? colors.green(result) : colors.red(result));
+      });
+    } else {
+      console.log('no results in component template directory');
+    }
+  });
+} else {
+  console.error(colors.red('\nYou must provide an argument like `--dir=<directory to podify components>`\n'));
+}
