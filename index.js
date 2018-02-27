0d8200ed09a24db3508e85985aab05394bd2c5f2
Justin Drew <justin.a.drew@gmail.com>
2018-02-26 19:08:28 -0700

    better code separation; reduce code duplication; reduce deps

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const colors = require('colors'),
-      figures = require('figures'),
-      fs = require('graceful-fs'),
-      mkdirp = require('mkdirp'),
-      path = require('path'),
-      mv = require('mv'),
-      args = require('minimist')(process.argv);
+
+const path = require('path'),
+      args = require('yargs-parser')(process.argv),
+      readDir = require('./lib/read-dir');
 
 console.log('starting...');
 
@@ -12,59 +9,8 @@ if(args.dir) {
   const componentDir = path.resolve(args.dir, 'app', 'components'),
       componentTemplates = path.resolve(componentDir, '..', 'templates', 'components');
 
-  fs.readdir(componentDir, function(err, list) {
-    if(err) {
-      console.log(colors.red('error reading component directory!\n'), err);
-    } else if(list) {
-      console.log('\nmoving component javascript...\n');
-
-      list.map((item) => {
-        let result = `${figures.tick} moved`;
-
-        if(item.indexOf('.js') > -1) {
-          mv(path.resolve(componentDir, item), path.resolve(componentDir, item.replace('.js', ''), 'component.js'), { mkdirp: true }, function(err) {
-            if(err) {
-              console.log(colors.red('error!'), err);
-            }
-          });
-        } else {
-          result = `${figures.cross} not moved`;
-        }
-
-        console.log(path.resolve(componentDir, item), result === 'moving...' ? colors.green(result) : colors.red(result));
-      });
-    } else {
-      console.log('no results in component directory');
-    }
-  });
-
-  fs.readdir(componentTemplates, function(err, list) {
-    if(err) {
-      console.log(colors.red('error reading component template directory!\n'), err);
-    } else if(list) {
-      console.log('\nmoving component templates...\n');
-
-      list.map((item) => {
-        let result = `${figures.tick} moved`;
-
-        if(item.indexOf('.hbs') > -1) {
-          mv(path.resolve(componentTemplates, item), path.resolve(componentDir, item.replace('.hbs', ''), 'template.hbs'), { mkdirp: true }, function(err) {
-            if(err) {
-              console.log(colors.red('template copy error!'), err);
-            }
-          });
-
-
-        } else {
-          result = `${figures.cross} not moved`;
-        }
-
-        console.log(path.resolve(componentTemplates, item), result === 'moving...' ? colors.green(result) : colors.red(result));
-      });
-    } else {
-      console.log('no results in component template directory');
-    }
-  });
+  readDir(componentDir);
+  readDir(componentTemplates);
 } else {
   console.error(colors.red('\nYou must provide an argument like `--dir=<directory to podify components>`\n'));
 }
